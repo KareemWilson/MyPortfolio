fad7761d40562b0f0cb6b36dbddd406e7aa97913
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-06 11:11:42 +0200

    Fix ESLint errors #2

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-let mobileMenu = document.querySelector(".nav-list")
-let humburger = document.querySelector('.openMenu')
-let closeMenu = document.querySelectorAll(".close-menu")
-let closeBtn = document.querySelector(".fa-close")
+let mobileMenu = document.querySelector(".nav-list");
+let humburger = document.querySelector(".openMenu");
+let closeMenu = document.querySelectorAll(".close-menu");
+let closeBtn = document.querySelector(".fa-close");
 
 const openMobileMenu = () => {
-    mobileMenu.style.display = 'block'
-    closeBtn.classList.remove('invisible')
-    humburger.classList.add("invisible")
-
-}
+  mobileMenu.style.display = 'block';
+  closeBtn.classList.remove('invisible');
+  humburger.classList.add('invisible');
+};
 
 const closeMobileMenu = () => {
-    mobileMenu.style.display = 'none'
-    humburger.classList.remove('invisible')
-    closeBtn.classList.add("invisible")
+  mobileMenu.style.display = 'none';
+  humburger.classList.remove('invisible');
+  closeBtn.classList.add('invisible');
+};
 
-}
-
-humburger.addEventListener('click', openMobileMenu)
-closeMenu.forEach(element => element.addEventListener('click', closeMobileMenu));
-
+humburger.addEventListener('click', openMobileMenu);
+closeMenu.forEach((element) =>
+  element.addEventListener('click', closeMobileMenu)
+);
