a0cb7f43186e55b12a859f9c135a1a5c7306330d
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-07 18:01:11 +0200

    Refactor folder structure

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
-const mobileMenu = document.querySelector('.nav-list');
-const humburger = document.querySelector('.openMenu');
-const closeMenu = document.querySelectorAll('.close-menu');
-const closeBtn = document.querySelector('.close');
-const popupBtn = document.querySelectorAll('.open-popup')
-const popup = document.querySelector('.popup')
+import './styles.css'
+import projectsData from './utils/data/data';
+
+
+
+const mobileMenu = document.querySelector(".nav-list");
+const humburger = document.querySelector(".openMenu");
+const closeMenu = document.querySelectorAll(".close-menu");
+const closeBtn = document.querySelector(".close");
+const popupBtn = document.querySelectorAll(".open-popup");
+const popup = document.querySelector(".popup");
+
 
 const openMobileMenu = () => {
-  mobileMenu.style.display = 'block';
-  closeBtn.classList.remove('invisible');
-  humburger.classList.add('invisible');
+  mobileMenu.style.display = "block";
+  closeBtn.classList.remove("invisible");
+  humburger.classList.add("invisible");
 };
 
 const closeMobileMenu = () => {
-  mobileMenu.style.display = 'none';
-  humburger.classList.remove('invisible');
-  closeBtn.classList.add('invisible');
+  mobileMenu.style.display = "none";
+  humburger.classList.remove("invisible");
+  closeBtn.classList.add("invisible");
 };
 
-humburger.addEventListener('click', openMobileMenu);
-closeMenu.forEach((element) => element.addEventListener('click', closeMobileMenu));
-
-
+humburger.addEventListener("click", openMobileMenu);
+closeMenu.forEach((element) =>
+  element.addEventListener("click", closeMobileMenu)
+);
 
 const openPopUp = () => {
-    popup.style.display = 'block'
-}
+  popup.style.display = "block";
+  console.log(popupBtn[0].id);
+};
 
 const closePopUp = (event) => {
-    if(event.target == popup){
-        popup.style.display = 'none' 
-        console.log(event.target);   
-    }
-    // popup.style.display = 'none'
-}
-popupBtn.forEach((element) => element.addEventListener('click', openPopUp ))
-closeBtn.addEventListener("click", () => popup.style.display = 'none')
-window.addEventListener('click', (event) => closePopUp(event))
+  if (event.target == popup) {
+    popup.style.display = "none";
+    console.log(event.target);
+  }
+};
+popupBtn.forEach((element) => element.addEventListener("click", openPopUp));
+closeBtn.addEventListener("click", () => (popup.style.display = "none"));
+window.addEventListener("click", (event) => closePopUp(event));
+
+
+//---------------------------------------
 
---
+++ utils/data/data.js
@@ -0,0 +1,50 @@
+ const projectsData = [
+    {
+      id: "project-1",
+      name: "project 1",
+      description: "smkcmlzdmlkfd",
+      img: "./utils/Snapshoot Portfolio.png",
+      technologies: ["Codekit", "JavaScript", "GitHub", "Bootstrap", "Codepen"],
+      linkToLive: "www.google.com",
+      sourceLink: "www.github.com",
+    },
+    {
+      id: "project-2",
+      name: "project 1",
+      description: "smkcmlzdmlkfd",
+      img: "./utils/Snapshoot Portfolio.png",
+      technologies: ["Codekit", "JavaScript", "GitHub", "Bootstrap", "Codepen"],
+      linkToLive: "www.google.com",
+      sourceLink: "www.github.com",
+    },
+    {
+      id: "project-3",
+      name: "project 1",
+      description: "smkcmlzdmlkfd",
+      img: "./utils/Snapshoot Portfolio.png",
+      technologies: ["Codekit", "JavaScript", "GitHub", "Bootstrap", "Codepen"],
+      linkToLive: "www.google.com",
+      sourceLink: "www.github.com",
+    },
+    {
+      id: "project-4",
+      name: "project 1",
+      description: "smkcmlzdmlkfd",
+      img: "./utils/Snapshoot Portfolio.png",
+      technologies: ["Codekit", "JavaScript", "GitHub", "Bootstrap", "Codepen"],
+      linkToLive: "www.google.com",
+      sourceLink: "www.github.com",
+    },
+    {
+      id: "project-5",
+      name: "project 1",
+      description: "smkcmlzdmlkfd",
+      img: "./utils/Snapshoot Portfolio.png",
+      technologies: ["Codekit", "JavaScript", "GitHub", "Bootstrap", "Codepen"],
+      linkToLive: "www.google.com",
+      sourceLink: "www.github.com",
+    },
+  ];
+
+
+  export default projectsData
