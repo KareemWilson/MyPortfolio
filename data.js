886a132aa10798674355ed578b75a2140d2d5493
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-08 10:31:08 +0200

    Add some css

--- data.js
+++ data.js
@@ -14,7 +14,7 @@ const projectsData = [
     name: "Multi-Post Stories Gain+Glory",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    img: "./utils/images/skills.png",
+    img: "./utils/images/Snapshoot Portfolio 2.png",
     technologies: ["Ruby on Rails", "JavaScript", "HTML", "CSS"],
     linkToLive: "www.google.com",
     sourceLink: "www.github.com",
