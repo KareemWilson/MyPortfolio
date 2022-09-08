3eda7c9bf99166ffd5b244683dd76deec0358d7f
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-08 10:26:22 +0200

    Fix Bug

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ import generateProjectCard from "./projectCard.js";
 const mobileMenu = document.querySelector(".nav-list");
 const humburger = document.querySelector(".openMenu");
 const closeMenu = document.querySelectorAll(".close-menu");
+const closeBtn = document.querySelector('.fa-close')
 
 const openMobileMenu = () => {
   mobileMenu.style.display = "block";
