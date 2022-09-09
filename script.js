d1e9b4098c05a8b642b44b7861e011e299ce5e75
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-09 16:03:52 +0200

    Make Custom Validation

--- script.js
+++ script.js
@@ -22,3 +22,53 @@ humburger.addEventListener('click', openMobileMenu);
 closeMenu.forEach((element) => element.addEventListener('click', closeMobileMenu));
 
 projectsData.forEach((project) => generateProjectCard(project));
+
+
+//-----------------------------------------
+const form = document.querySelector('#contact')
+const email = document.querySelector('#mail')
+const error = document.querySelector('#mail + .error')
+const upperCaseCheck = () => {
+  if(email.value != email.value.toLowerCase()){
+    return true
+  }
+  else{
+    return false
+  }
+}
+
+const showError = () => {
+  
+  if(email.validity.valueMissing){
+    error.textContent = 'You should type your mail'
+  }
+  else if(email.validity.typeMismatch){
+    error.textContent = 'You should enter email address'
+  }
+  if(upperCaseCheck()){
+    error.textContent = 'your Email should be in lowercase'
+  }
+
+  error.className = 'error active'
+}
+
+
+email.addEventListener('input', () => {
+  if(email.validity.valid){
+    error.textContent = ''
+    error.className = 'error'
+  }
+  else {
+    showError()
+  }
+  console.log(email.value);
+  console.log(upperCaseCheck());
+})
+
+form.addEventListener('submit', (event) => {
+  if(!email.validity.valid){
+    showError()
+    event.preventDefault()
+  }
+  
+})
